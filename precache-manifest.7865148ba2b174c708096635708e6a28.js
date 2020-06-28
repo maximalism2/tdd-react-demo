694c6bc172108dec4db69c6498d8f029fdd0f0df
necinc <mvyznyuk@gmail.com>
2020-06-28 10:45:11 +0000

    Deploying to gh-pages from  @ 2efbb2ffe84e928e1223a6d680ac43f3078f65b2 🚀

---
+++ precache-manifest.7865148ba2b174c708096635708e6a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b65251a97d01e0d65b5e4f9c14c5d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44b29f11b52e2eab1555",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8a5ee4e0aa61a11dc202",
+    "url": "/static/js/2.c94df3e4.chunk.js"
+  },
+  {
+    "revision": "1d8dfa16d11c0ca8f44c5b5214fef4d3",
+    "url": "/static/js/2.c94df3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b29f11b52e2eab1555",
+    "url": "/static/js/main.944696c6.chunk.js"
+  },
+  {
+    "revision": "2878b58649236c03f3e7",
+    "url": "/static/js/runtime-main.7189ac94.js"
+  }
+]);
